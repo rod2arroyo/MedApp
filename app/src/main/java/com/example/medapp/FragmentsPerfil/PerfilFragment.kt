a882f9f45e4417446aabf269ff8e0600fe434d1e
rod2arroyo <20180138@aloe.ulima.edu.pe>
2021-12-05 05:36:23 -0500

    Ya termine toda mi muy parte ps

--- app/src/main/java/com/example/medapp/FragmentsPerfil/PerfilFragment.kt
+++ app/src/main/java/com/example/medapp/FragmentsPerfil/PerfilFragment.kt
@@ -1,12 +1,16 @@
 package com.example.medapp.FragmentsPerfil
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.fragment.app.Fragment
 import com.example.medapp.FragmentsCitas.AgregarCitaFragment
+import com.example.medapp.LoginActivity
 import com.example.medapp.R
 
 class PerfilFragment  : Fragment(){
@@ -22,6 +26,7 @@ class PerfilFragment  : Fragment(){
         super.onViewCreated(view, savedInstanceState)
         val btnEditarDatos = view.findViewById<Button>(R.id.btn_editar_datos)
         val btnEditarFoto = view.findViewById<Button>(R.id.btn_editar_foto)
+        val btnLogout = view.findViewById<Button>(R.id.btn_desconectarse)
 
         btnEditarDatos.setOnClickListener{
             val fragment = EditarPerfilFragment()
@@ -37,6 +42,11 @@ class PerfilFragment  : Fragment(){
                 ?.replace(R.id.frame_container, fragment, "fragmnetId")
                 ?.addToBackStack(null)
                 ?.commit();
+        }
+
+        btnLogout.setOnClickListener{
+            val intent = Intent(this.requireContext(), LoginActivity::class.java)
+            startActivity(intent)
         }
     }
 }
