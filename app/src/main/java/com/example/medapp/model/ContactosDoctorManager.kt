98e8ea2079dd2a2f5524b6a68a1ca6d73b6147c3
rod2arroyo <20180138@aloe.ulima.edu.pe>
2021-12-04 19:56:46 -0500

    Eliminar duplicidad

--- app/src/main/java/com/example/medapp/model/ContactosDoctorManager.kt
+++ app/src/main/java/com/example/medapp/model/ContactosDoctorManager.kt
@@ -3,34 +3,28 @@ package com.example.medapp.model
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
-class CitasManager {
+class ContactosDoctorManager {
     companion object{
-        var instance : CitasManager = CitasManager()
+        var instance : ContactosDoctorManager = ContactosDoctorManager()
             private set
     }
 
 
     private val dbFirebase = Firebase.firestore
 
-    fun createCitas(usuario: String,
+    fun createContactoDoctor(usuario: String,
                  nombre: String,
-                 doctor: String,
                  especialidad: String,
-                 horario: String,
-                 //callbackOK: (Long) -> Unit,
-                 //callbackError: (String) -> Unit
+                 ubicacion: String,
     ){
 
         val data = hashMapOf<String,Any>(
             "nombre" to nombre,
-            "doctor" to doctor,
             "especialidad" to especialidad,
-            "horario" to horario,
+            "ubicacion" to ubicacion
         )
-
         val userId = usuario
-
-        dbFirebase.collection("Citas")
+        dbFirebase.collection("ContactosMedicos")
             .document(userId)
             .set(data)
             .addOnSuccessListener {
